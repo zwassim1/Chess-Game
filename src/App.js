6b2c1ac5f137b239735ea23b317dddf58cb1cd35
Wassim Zouaoui <wzouaoui@dom.tti>
2019-07-18 10:49:45 +0100

    chess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Board from './Components/Board';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ width: '100%', height: '100%' }}>
+			<Board knightPosition={[0, 0]} />
+		</div>
+	);
 }
 
 export default App;
